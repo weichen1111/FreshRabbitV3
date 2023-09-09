51acd927728b4baf1b9bd51b80e1b8cb7ef7724f
weichen <3311018318@qq.com>
2023-09-09 15:31:37 +0800

    详情页-小图切换大图显示

--- vue-rabbit/src/views/Detail/index.vue
+++ vue-rabbit/src/views/Detail/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 // 引入detail接口
 import DetailHot from './components/DetailHot.vue';
+import ImageView from '@/components/ImageView/index.vue'
 import { getDetail } from '@/apis/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
@@ -44,7 +45,7 @@ onMounted(() => getGoods())
                     <div class="goods-info">
                         <div class="media">
                             <!-- 图片预览区 -->
-
+                            <ImageView />
                             <!-- 统计数量 -->
                             <ul class="goods-sales">
                                  <li>
