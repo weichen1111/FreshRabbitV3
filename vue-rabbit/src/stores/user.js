9f46628ad3b6e53d614c6f1676395d5c123a14a3
weichen <3311018318@qq.com>
2023-09-12 15:11:27 +0800

    登录-退出登录实现

--- vue-rabbit/src/stores/user.js
+++ vue-rabbit/src/stores/user.js
@@ -12,10 +12,15 @@ export const useUserStore =  defineStore('user',()=> {
        const res = await  loginAPI({account,password})
        userInfo.value = res.result
     }
+    // 退出时清楚用户信息
+    const clearUserInfo = () => {
+        userInfo.value = {}
+    }
     // 3.以对象格式return出去
     return{
         userInfo,
-        getUserInfo
+        getUserInfo,
+        clearUserInfo
     }
 }, {
     persist: true,
