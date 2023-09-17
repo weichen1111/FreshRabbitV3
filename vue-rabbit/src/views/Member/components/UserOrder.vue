eed40588a34ad2a7721adce6bb56343d3da6a426
weichen <3311018318@qq.com>
2023-09-17 10:36:38 +0800

    订单-分页逻辑

--- vue-rabbit/src/views/Member/components/UserOrder.vue
+++ vue-rabbit/src/views/Member/components/UserOrder.vue
@@ -14,6 +14,8 @@ const tabTypes = [
 ]
 // 订单列表
 const orderList =ref([]) 
+//存入分页总数
+const total =ref (0)
 const params= ref({
     orderState: 0,
     page: 1,
@@ -22,6 +24,8 @@ const params= ref({
 const getOrderList =async() =>{
     const res = await getUserOrder(params.value)
     orderList.value= res.result.items 
+    //分页赋值
+    total.value =res.result.counts
 }
 onMounted(() => getOrderList())
 
@@ -29,6 +33,13 @@ onMounted(() => getOrderList())
 const tabChange =(type) =>{
     // console.log(type);
     params.value.orderState =type
+    getOrderList()
+}
+
+//页数切换
+const pageChange =(page) =>{
+    // console.log(page);
+    params.value.page = page
     getOrderList()
 }
 </script>
@@ -112,7 +123,7 @@ const tabChange =(type) =>{
                     </div>
                     <!-- 分页 -->
                     <div class="pagination-container">
-                        <el-pagination background layout="prev, pager, next" />
+                        <el-pagination :total="total" @current-change="pageChange" :page-size="params.pageSize" background layout="prev, pager, next" />
                     </div>
                 </div>
             </div>
